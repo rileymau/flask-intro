8fccbe7f363b9a87d1323f8f3c8abd87621d5f3a
Molly Riley <mo.riley@gmail.com>
2021-10-12 17:19:36 +0000

    recommit

---
+++ server.py
@@ -0,0 +1,88 @@
+"""Greeting Flask app."""
+
+from random import choice
+
+from flask import Flask, request
+
+# "__name__" is a special Python variable for the name of the current module
+# Flask wants to know this to know what any imported things are relative to.
+app = Flask(__name__)
+
+AWESOMENESS = [
+    'awesome', 'terrific', 'fantastic', 'neato', 'fantabulous', 'wowza',
+    'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
+    'wonderful', 'smashing', 'lovely']
+
+# tried compliment_dropdown = []
+#for compliment in AWESOMENESS:
+#  compliment_dropdown.append(f'<option value="{compliment}">{compliment}</option>')
+
+@app.route('/')
+def start_here():
+    """Home page."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Start Here</title>
+      </head>
+      <body>
+      It's our homepage:
+        <a href="/hello">Click here to start</a>
+      </body>
+    </html>
+    """
+
+@app.route('/hello')
+def say_hello():
+    """Say hello and prompt for user's name."""
+
+    return """
+    <!doctype html>
+    <html>
+      <head>
+        <title>Hi There!</title>
+      </head>
+      <body>
+        <h1>Hi There!</h1>
+        <form action="/greet">
+          What's your name? <input type="text" name="person">
+          <select name="compliments">
+            <option value="you're nice">You're Nice</option>
+            <option value="awesome">Awesome</option>
+            <option value="terrific">Terrific</option>
+
+          <input type="submit" value="Submit">
+        </form>
+      </body>
+    </html>
+    """
+
+
+@app.route('/greet')
+def greet_person():
+    """Get user by name."""
+
+    player = request.args.get("person")
+
+    #was: compliment = choice(AWESOMENESS)
+    compliment = request.args.get("compliments")
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Compliment</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {compliment}!
+      </body>
+    </html>
+    """
+
+
+if __name__ == '__main__':
+    # debug=True gives us error messages in the browser and also "reloads"
+    # our web app if we change the code.
+    app.run(debug=True, host="0.0.0.0")
